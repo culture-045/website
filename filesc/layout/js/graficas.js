2bd9e3bb98ca524b680b272dea303f2eaada9e21
yenoth <80509534+yenoth@users.noreply.github.com>
2021-10-28 23:04:28 -0500

    website :v

---
+++ filesc/layout/js/graficas.js
@@ -0,0 +1,31 @@
+// Obtener una referencia al elemento canvas del DOM
+const $grafica = document.querySelector("#grafica");
+// Las etiquetas son las que van en el eje X. 
+const etiquetas = ["Semana 1", "Semana 2", "Semana 3", "Semana 4"]
+// Podemos tener varios conjuntos de datos. Comencemos con uno
+const datosVentas2020 = {
+    label: "Progreso por semana",
+    data: [0, 50, 75, 100], // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
+    backgroundColor: 'rgba(54, 162, 235, 0.2)', // Color de fondo
+    borderColor: 'rgba(54, 162, 235, 1)', // Color del borde
+    borderWidth: 3,// Ancho del borde
+};
+new Chart($grafica, {
+    type: 'line',// Tipo de gráfica
+    data: {
+        labels: etiquetas,
+        datasets: [
+            datosVentas2020,
+            // Aquí más datos...
+        ]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }],
+        },
+    }
+});
